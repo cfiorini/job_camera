e7d61eaa535232262402e88c466b8db24e51b6f1
Claudio Fiorini <claudio.fiorini@gmail.com>
2015-05-12 11:39:15 +0200

    perform_later test

--- spec/integration/ciao_job_spec.rb
+++ spec/integration/ciao_job_spec.rb
@@ -15,6 +15,22 @@ describe CiaoJob do
       expect(JobCamera::JobCameraLog.count).to be > 0
     end
 
+  end
+
+  describe '.perform_later' do
+
+    it 'log enqueue' do
+      a = jc.perform_later([])
+      job_id = a.job_id
+      queue_name = a.queue_name
+      class_name = a.class.to_s
+
+      res = JobCamera::JobCameraLog.find_by_job_id_and_queue_name_and_job_name(job_id,
+                                                                         queue_name,
+                                                                         class_name)
+
+      expect(res).to be_kind_of(JobCamera::JobCameraLog)
+    end
 
   end
 
